82809c18e77df13f73e543fd19e7188cb52f8a9c
Jerry Yen <jerry160726@gmail.com>
2022-03-04 00:28:41 +0800

    訂單送出

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,18 +1,49 @@
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+// import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 //Base URL: 用這個改各方法需要的URL
 const apiPath = 'ttest';  //自己的api_path
 
-const app = createApp({
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+
+loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+configure({
+  generateMessage: localize('zh_TW'),
+});
+
+const app = Vue.createApp({
     data() {
         return {
-            cartData: {},
+            cartData: {
+                carts: []   //清空購物車才可以用disabled
+            },
+            form: {
+                user: {
+                  name: '',
+                  email: '',
+                  tel: '',
+                  address: '',
+                },
+                message: '',
+              },
             products: [],
             productId: '',
             isLoadingItem: '', //新增讀取效果
         };
     },
+    components: {
+        VForm: Form,
+        VField: Field,
+        ErrorMessage,
+      },
     methods: {
         getProducts() {
             axios.get(`${apiUrl}/api/${apiPath}/products/all`)
@@ -44,7 +75,7 @@ const app = createApp({
                     // 做完即關閉視窗
                     this.isLoadingItem = '';
                 })
-                .catch((err)=>{
+                .catch((err) => {
                     console.log(err.data);
                 });
         },
@@ -69,6 +100,17 @@ const app = createApp({
                     this.isLoadingItem = '';
                 });
         },
+        createOrder() {
+            const url = `${apiUrl}/api/${apiPath}/order`;
+            const order = this.form;
+            axios.post(url, { data: order }).then((response) => {
+              alert(response.data.message);
+              this.$refs.form.resetForm();
+              this.getCartData();
+            }).catch((err) => {
+              alert(err.data.message);
+            });
+          },
     },
     mounted() {
         this.getProducts();
@@ -96,7 +138,7 @@ app.component('product-modal', {
         openModal() {
             this.modal.show();
         },
-        closeModal(){
+        closeModal() {
             this.modal.hide();
         },
         getProduct() {
@@ -108,10 +150,8 @@ app.component('product-modal', {
         },
         addToCart() {
             console.log(this.qty);
-            console.log(typeof(this.qty));
-            const qty = parseInt(this.qty);
-            console.log(typeof(qty));
-            this.$emit('add-cart', this.product.id, qty);
+            console.log(typeof (this.qty));
+            this.$emit('add-cart', this.product.id, this.qty);
         },
     },
     mounted() {
@@ -119,4 +159,7 @@ app.component('product-modal', {
     },
 });
 
+
 app.mount('#app');
+
+
